30607322bf588fb6a153795ca11f1be5cd25eebf
Brittanie Morgan <brittaniemorgan12345@gmail.com>
2022-10-29 15:53:48 -0500

    Added new webpage

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
-/* Add your JavaScript to this file */
+window.onload = function(){
+    
+    const emailExp = new RegExp("[a-z0-9]+@[a-z]+\.[a-z]{2,3}");
+    function subcribeAction(e){
+        e.preventDefault();
+        let emailInput = document.getElementById("email").value;
+        console.log(emailExp.test(emailInput));
+        console.log(emailInput);
+        if ( emailInput == ""   ||  !emailInput.match(emailExp)){
+            document.querySelector(".message").innerHTML = "Please enter a valid email address."; 
+        }
+        else {
+            document.querySelector(".message").innerHTML = `Thank you! Your email address ${emailInput} has been added to our mailing list!`;
+        }
+    }
+    
+    var submitButton = document.querySelector("button");
+    submitButton.addEventListener("click", subcribeAction);
+}
